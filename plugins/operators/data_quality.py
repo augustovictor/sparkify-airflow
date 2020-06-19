79e8bb7afce6daf502d4a3d7fe80dd2ff1d9352a
Victor Costa <victor.costa@loft.com.br>
2020-06-18 21:07:44 -0300

    feat: Add data quality step

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -1,4 +1,5 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -8,15 +9,27 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 conn_id: str,
+                 aws_credentials_id: str,
+                 table: str,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+
+        self.aws_credentials = AwsHook(aws_conn_id=aws_credentials_id).get_credentials()
+        self.redshift = PostgresHook(postgres_conn_id=conn_id)
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('DataQualityOperator step')
+        formatted_dql = f"SELECT COUNT(*) FROM {self.table}"
+        result = self.redshift.get_records(formatted_dql)
+
+        if len(result) < 1 or len(result[0]) < 1:
+            raise ValueError("Actual result from validation task does not match expected result")
+
+        num_records = result[0][0]
+
+        if num_records < 1:
+            raise ValueError(
+                "Actual result from validation task does not match expected result")
